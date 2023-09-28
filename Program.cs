d4f4fa2cc01fc05621e8cd70adc3963c7a1e8e91
FatimaRasool24 <146317455+FatimaRasool24@users.noreply.github.com>
2023-09-28 10:38:07 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+
+namespace Domain1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         try { 
+                Console.WriteLine("How many cars do you want?");
+                int amount = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Your order total will be {0} dollars", amount*16);
+         } 
+        catch (Exception ex)
+        {
+                Console.WriteLine(ex.Message);
+        }
+        GC.Collect();
+        Console.WriteLine("Memory used: {0}", GC.GetTotalMemory(true));
+        }
+    } 
+ }
+    
+
+    
